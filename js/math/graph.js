e47d7f19e612b36af30ebf76c7614670404572bd
John <johncarnagey14@gmail.com>
2024-01-17 20:40:42 -0600

    updated

--- js/math/graph.js
+++ js/math/graph.js
@@ -1,16 +1,48 @@
 class Graph {
-    constructor(points = [], segments = []) {
-        this.points = points;
-        this.segments = segments;
+  constructor(points = [], segments = []) {
+    this.points = points;
+    this.segments = segments;
+  }
+
+  addPoint(point) {
+    this.points.push(point);
+  }
+
+  containsPoint(point) {
+    return this.points.find((p) => p.equals(point));
+  }
+
+  tryAddPoint(point) {
+    if (!this.containsPoint(point)) {
+      this.addPoint(point);
+      return true;
+    }
+    return false;
+  }
+
+  addSegment(seg) {
+    this.segments.push(seg);
+  }
+
+  containSegment(seg) {
+    return this.segments.find((s) => s.equals(seg));
+  }
+
+  tryAddSegment(seg) {
+    if (!this.containSegment(seg)) {
+      this.addSegment(seg);
+      return true;
+    }
+    return false;
+  }
+
+  draw(ctx) {
+    for (const seg of this.segments) {
+      seg.draw(ctx);
     }
 
-    draw(ctx) {
-        for (const seg of this.segments) {
-            seg.draw(ctx);
-        }
-
-        for (const point of this.points) {
-            point.draw(ctx);
-        }
+    for (const point of this.points) {
+      point.draw(ctx);
     }
+  }
 }
