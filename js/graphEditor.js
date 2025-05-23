0bd13e2d7816ca33dc673db1c7c43ba5ec9c7eb8
jcarnagey <johncarnagey14@gmail.com>
2025-05-23 12:58:46 -0500

    added point delete functionality

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -13,21 +13,37 @@ class GraphEditor {
 
     #addEventListeners() {
         this.canvas.addEventListener("mousedown", (evt) => {
-            const mouse = new Point(evt.offsetX, evt.offsetY);
-            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
-            if (this.hovered) {
-                this.selected = this.hovered;
-                return;
+            if (evt.button == 2) { //right click
+                if (this.hovered) {
+                    this.#removePoint(this.hovered);
+                }
             }
-            this.graph.addPoint(mouse);
-            this.selected = mouse;
+            if (evt.button == 0) { //left click
+                const mouse = new Point(evt.offsetX, evt.offsetY);
+                if (this.hovered) {
+                    this.selected = this.hovered;
+                    return;
+                }
+                this.graph.addPoint(mouse);
+                this.selected = mouse;
+                this.hovered = mouse;
+            }
         });
 
         this.canvas.addEventListener("mousemove", (evt) => {
             const mouse = new Point(evt.offsetX, evt.offsetY);
             this.hovered = getNearestPoint(mouse, this.graph.points, 10);
         });
+        this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
     }
+
+    #removePoint(point) {
+        this.graph.removePoint(point);
+        this.hovered = null;
+        if (this.selected == point) {
+            this.selected = null;
+        }
+    };
 
     display() {
         this.graph.draw(this.ctx);
